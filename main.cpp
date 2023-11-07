95c5ebe6f2de95dc41b037400ac5b9ea9c6272ee
sergiioo101 <smartros@myuax.com>
2023-11-07 11:30:23 +0100

    comentarios 3

--- main.cpp
+++ main.cpp
@@ -3,13 +3,19 @@
 #include <cmath>
 
 int main() {
-    int limite = 20; // Puedes ajustar este valor para buscar triples pitagóricos en un rango diferente
+    int limite = 20; // Limite para "a" y "b", que puedes ajustar para cambiar el rango de busqueda
 
+    // Bucle para "a", primer lado del triangulo
     for (int a = 1; a <= limite; a++) {
+        // Bucle para "b", segundo lado del triangulo
         for (int b = a; b <= limite; b++) {
+            // Calcula el tercer lado "c", usando la fórmula de Pitagoras.
             int c = std::sqrt(a * a + b * b);
+            // Verifica si el conjunto de lados (a, b, c) forma un triple pitagorico valido
+            // Verifica si cumple con las condiciones de un triangulo rectangulo.
 
             if (esTriplePitagorico(a, b, c) && esTrianguloValido(a, b, c)) {
+                // Si se cumplen las condiciones, imprime el triple pitagorico encontrado.
                 std::cout << a << " - " << b << " - " << c << std::endl;
             }
         }
