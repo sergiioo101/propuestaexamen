44485c98e83c7962b1a89562f1f2a3bd2c58cef1
sergiioo101 <smartros@myuax.com>
2023-11-07 11:09:54 +0100

    comentarios 1

--- funciones.cpp
+++ funciones.cpp
@@ -2,6 +2,9 @@
 bool esTriplePitagorico(int a, int b, int c) {
     return a * a + b * b == c * c;
 }
+// Funcion para verificar si un conjunto de tres numeros representa un triple pitagorico
+// Esta recibe tres numeros positivos enteros a, b y c
+// Devuelve un valor "true" si los 3 valores forman un triple pitagorico
 
 bool esTrianguloValido(int a, int b, int c) {
     return a + b > c && a + c > b && b + c > a;
